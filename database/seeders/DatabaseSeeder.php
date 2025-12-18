603d9c0ed3cac2fba76fa2736628cb5d6157404a
devWebs01 <testingbae66@gmail.com>
2025-12-18 23:36:48 +0700

    add filament and role permission plugin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +17,23 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+    { // 1. Pastikan role super_admin ada
+        $superAdminRole = Role::firstOrCreate([
+            'name' => 'super_admin',
+            'guard_name' => 'web',
         ]);
+
+        // 2. Buat / ambil user admin
+        $user = User::firstOrCreate(
+            ['email' => 'admin@testing.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'), // ganti di production
+            ]
+        );
+
+        // 3. Assign role jika belum
+        $permissions = Permission::pluck('name')->toArray();
+        $user->syncPermissions($permissions);
     }
 }
