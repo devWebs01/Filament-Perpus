c7189b5d2991277e75dce0a04f834fcb275830df
devWebs01 <testingbae66@gmail.com>
2025-12-27 16:34:14 +0700

    Please provide the files to generate a commit message.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -19,6 +19,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -58,6 +59,12 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 FilamentShieldPlugin::make(),
+                FilamentEditProfilePlugin::make()
+                    ->setTitle('Profile Akun')
+                    ->setNavigationLabel('Profile Akun')
+                    ->setNavigationGroup('Manajemen Pengguna')
+                    ->setIcon('heroicon-o-user'),
+
             ]);
     }
 }
